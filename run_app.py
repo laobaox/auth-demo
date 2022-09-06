8a9c4c0f57361cbec0b547db596cfb10f9c72e00
bao <thevoid@163.com>
2022-09-06 10:10:58 +0800

    update Dockerfile

--- run_app.py
+++ run_app.py
@@ -3,7 +3,7 @@ from auth_demo.app import app
 from auth_demo import config
 
 def main():
-    uvicorn.run(app, host=config.get_host(), port=config.get_port())
+    uvicorn.run(app, port=config.get_port())
 
 if __name__ == '__main__':
     main()
